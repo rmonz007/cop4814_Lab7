86db3b016f77cdd69bd72cc082572f46ed03c56f
rmonz007 <37599364+rmonz007@users.noreply.github.com>
2018-03-20 17:55:48 -0400

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Game
+    {
+        public string Team1;
+        public string Team2;
+        public int Team1Score;
+        public int Team2Score;
+
+        public Game()
+        {
+
+        }
+
+        public Game(string Team1, string Team2, int Team1Score, int Team2Score)
+        {
+            this.Team1 = Team1;
+            this.Team2 = Team2;
+            this.Team1Score = Team1Score;
+            this.Team2Score = Team2Score;
+        }
+
+        public override string ToString ()
+        {
+            return Team1 + " (" + Team1Score + ") - " + Team2 + " (" + Team2Score + ")";
+        }
+    }
+}
---
+++ GameFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Lab2
+{
+    class GameFactory
+    {
+        List<Game> gamelist = new List<Game>();
+        public string FilePath = "games.xml";
+        public GameFactory()
+        {
+
+        }
+
+        public void CreateGameList()
+        {
+            gamelist.Add(new Game("Lions", "Bears", 0, 1));
+            gamelist.Add(new Game("Dolphins", "Lions", 6, 12));
+            gamelist.Add(new Game("Bears", "Dolphins", 0, 0));
+            gamelist.Add(new Game("Tigers", "Dolphins", 12, 16));
+            gamelist.Add(new Game("Tigers", "Bears", 7, 18));
+            gamelist.Add(new Game("Lions", "Tigers", 18, 26));
+
+        }
+
+        public Boolean SerializeGameList()
+        {
+            StreamWriter sw = new StreamWriter(FilePath);
+            XmlSerializer ser = new XmlSerializer(gamelist.GetType());
+            ser.Serialize(sw, gamelist);
+            sw.Close();
+            return true;
+        }
+    }
+}
